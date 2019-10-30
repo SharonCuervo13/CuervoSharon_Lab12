20cf46e545c4d1db9c15aafee2d77cfa78252890
SharonCuervo13 <56890913+SharonCuervo13@users.noreply.github.com>
2019-10-30 18:32:24 -0500

    Add files via upload

--- uno.cpp
+++ uno.cpp
@@ -5,5 +5,5 @@ int main(){
     std::cin>>r;
     std::cout <<"diametro:"<<2*r<<std::endl;
     std::cout <<"perimetro:"<<2*3.141592*r<<std::endl;
-
+    std::cout <<"area:"<<3.141592*r*r<<std::endl;
 }
