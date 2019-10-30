b675196278197d6f1cb52de7e310dd9fdb849ade
SharonCuervo13 <56890913+SharonCuervo13@users.noreply.github.com>
2019-10-30 18:38:23 -0500

    Add files via upload

--- dos.cpp
+++ dos.cpp
@@ -6,5 +6,10 @@ int main(){
     float z;
     std::cout <<"Ingrese lados del triangulo"<<std::endl;
     std::cin>>x>>y>>z;
- 
+    if(sqrt(x*x+y*y=z*z)||sqrt(x*x+z*z=y*y)||sqrt(z*z+y*y=x*x)){
+       std::cout <<"Es un triángulo rectangulo"<<std::endl; 
+    }
+    else{
+       std::cout <<"No es un triángulo rectangulo"<<std::endl;
+    return 0;
 }
